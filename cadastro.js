c6361096d7c4e106492498a3107cc9b0ac1eedb7
Guilherme Camargo <gwcamargo20@gmail.com>
2023-07-19 10:49:56 -0300

    testando regex

--- cadastro.js
+++ cadastro.js
@@ -26,8 +26,9 @@
         }
         
         // verificar se o telefone está preenchido corretamente e se é válido
-        if (telefone.value === "" || !validarTelefone(telefone.value)) {
+        if (telefone.value === "" || !validarNumeroTelefone(telefone.value)) {
             alert("Número inválido, tente novamente")
+            return
         }
 
         // se todos os campos estiverem corretamente preenchidos, envie o form
@@ -48,18 +49,36 @@
         }
     }
 
-    // função para validar telefone
-    function validarTelefone (telefone) {
+     // função para validar telefone
+     //function validarTelefone (telefone) {
         // criar uma regex para validar telefone
-        const telRegex = new RegExp(
-            /^([14689][0-9]|2[12478]|3([1-5]|[7-8])|5([13-5])|7[193-7])9[0-9]{8}$/
-        )
+        //const telRegex = new RegExp(
+          //  /^([14689][0-9]|2[12478]|3([1-5]|[7-8])|5([13-5])|7[193-7])9[0-9]{8}$/
+        //)
 
-        if (telRegex.test(telefone)) {
-            return true
-        } else {
-            return false
-        }
+        //if (telRegex.test(telefone)) {
+            //return true
+        //} else {
+            //return false
+        //}
+    //}
+        
+    const regex = /^(?:(?:\+|00)?(55)\s?)?(?:\(?([1-9][0-9])\)?\s?)?(?:((?:9\d|[2-9])\d{3})\-?(\d{4}))$/;
+
+    function validarNumeroTelefone(numero) {
+      if (regex.test(numero)) {
+        console.log("Número de telefone válido!");
+      } else {
+        console.log("Número de telefone inválido!");
+      }
     }
-
     
+
+    // Exemplos de números de telefone para teste
+    validarNumeroTelefone("(12) 3456-7890"); // Número válido
+    validarNumeroTelefone("(12) 34567890"); // Número válido
+    validarNumeroTelefone("123456-7890"); // Número válido
+    validarNumeroTelefone("+55 12 3456-7890"); // Número válido
+    validarNumeroTelefone("00 55 (12) 3456-7890"); // Número válido
+    validarNumeroTelefone("1234567890"); // Número inválido
+    validarNumeroTelefone("12 34567-8901"); // Número inválido
