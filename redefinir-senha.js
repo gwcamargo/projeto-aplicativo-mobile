e402d4083dcbf9d482b115335276b1c7899a3f8f
Guilherme Camargo <gwcamargo20@gmail.com>
2023-07-27 16:53:36 -0300

    correção redefinir senha

--- redefinir-senha.js
+++ redefinir-senha.js
@@ -1,13 +1,24 @@
 const FormRedefinirSenha = document.querySelector(".form-redefinicao-senha")
-const RedefinirSenha = document.querySelector(".redefenir-senha")
-const ConfirmeSuaSenha = document.querySelector("#confirme-sua-senha")
+const RedefinirSenha = document.getElementById("redefinir-senha")
+const ConfirmeSuaSenha = document.getElementById("confirme-sua-senha")
 const btnSalvarSenha = document.querySelector(".btn-salvar-senha")
 
 FormRedefinirSenha.addEventListener("submit", (event) => {
     event.preventDefault()
 
-    if (RedefinirSenha.value === "" || !validaSenha(RedefinirSenha.value, 8)) {
+    if (RedefinirSenha.value === "") {
         alert("Por favor, redefine sua senha!")
+        return   
+    } else if (!validaSenha(RedefinirSenha.value, 8)) {
+        alert("Sua senha precisa ter no mínimo 8 caracteres!")
+        return
+    }
+
+    if (ConfirmeSuaSenha.value === "") {
+        alert("Por favor, confirme sua senha!")
+        return
+    } else if (ConfirmeSuaSenha.value !== RedefinirSenha.value) {
+        alert("Sua senha é diferente!")
         return
     }
 
@@ -15,7 +26,7 @@ FormRedefinirSenha.addEventListener("submit", (event) => {
 })
 
 function validaSenha(RedefinirSenha, minDigits) {
-    if (RedefinirSenha >= minDigits) {
+    if (RedefinirSenha.length >= minDigits) {
         return true
     } else {
         return false
