4db719a28cca1a9d82fbaf03641ed6ef44a39458
Guilherme Camargo <gwcamargo20@gmail.com>
2023-11-30 11:38:47 -0300

    date field

--- tela-principal.js
+++ tela-principal.js
@@ -2,7 +2,7 @@ import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
 
-import { getFirestore, collection, addDoc, getDoc, doc, query, getDocs, where } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
+import { getFirestore, collection, addDoc, getDoc, doc, query, getDocs, where, deleteDoc } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
 
 const address = document.querySelector("#endereço")
 const houseNumber = document.querySelector("#house-number")
@@ -34,6 +34,14 @@ auth.onAuthStateChanged((user) => {
     
 })
 
+async function excluirDados(docid) {
+    await deleteDoc(doc(db, "localização do morador", docid)).then((response)=>{
+    console.log(response)
+    }).catch((error)=>{
+        console.log(error)
+    })
+}
+
 // Initialize Cloud Firestore and get a reference to the service
 async function carregaTabela(){
     const db = getFirestore(aplication);
@@ -42,44 +50,31 @@ async function carregaTabela(){
     const querySnapshot = await getDocs(q)
 
     locationTable.innerHTML += "<table>"
-    locationTable.innerHTML += "<thead><th>endereço</th> <th>número da casa</th> <th>tipo de lixo</th> <th>data</th> <th>hora</th></thead>"
+    locationTable.innerHTML += "<thead><th>endereço</th> <th>número da casa</th> <th>tipo de lixo</th> <th>data</th><th>excluir</th></thead>"
     querySnapshot.forEach((doc) => {
 
-        var row = locationTable.insertRow(1);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2)
-        var cell4 = row.insertCell(3)
-        var cell5 = row.insertCell(4)
+        var row = locationTable.insertRow();
+        var cell0 = row.insertCell(0);
+        var cell1 = row.insertCell(1);
+        var cell2 = row.insertCell(2)
+        var cell3 = row.insertCell(3)
+        var cell4 = row.insertCell(4)
 
-        cell1.innerHTML = doc.data().address;
-        cell2.innerHTML = doc.data().houseNumber;
-        cell3.innerHTML = doc.data().tipoLixo
-        cell4.innerHTML = doc.data().data
-        cell5.innerHTML = doc.data().hour
+        cell0.innerHTML = doc.data().address
+        cell1.innerHTML = doc.data().houseNumber
+        cell2.innerHTML = doc.data().tipoLixo
+        cell3.innerHTML = doc.data().data
+        
+        var bt = document.createElement("button")
+        bt.id=doc.id
+        
+        bt.innerHTML="EXCLUIR"
+        cell4.appendChild(bt)
+        bt.onclick = function(){
+            excluirDados(doc.id)
+            locationTable.deleteRow(cell4.parentNode.rowIndex)
+        }
     })
-}
-
-// função para pegar data e hora atual
-function getDateAndHour() {
-    // Data
-    const date = new Date()
-    const dayweek = date.getDay()
-
-    const day = date.getDate()
-    const month = date.getMonth()
-    const year = date.getFullYear()
-
-    // Hora
-    const hour = date.getHours()
-    const minutes = date.getMinutes()
-    const seconds = date.getSeconds()
-
-    const monthArray = new Array("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro")
-
-    const dayweekArray = new Array("Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado")
-
-    document.write(dayweekArray[dayweek] + "," + day + "de" + monthArray[month] + "de" + year)
 }
 
 const db = getFirestore(aplication);
@@ -98,24 +93,15 @@ btnSaveLocation.addEventListener("click", (event) => {
         return
     }
 
-    alert(tipoLixoCheckbox.value)
-
-    if (tipoLixoCheckbox.value) {
-        alert("Marque o tipo do lixo")
-        console.log(tipoLixoCheckbox)
-    }
-
     addDoc(collection(db, 'localização do morador'), {
         uid: auth.currentUser.uid,
         address: address.value,
         houseNumber: parseInt(houseNumber.value),
-        tipoLixo: tipoLixoCheckbox.value
+        tipoLixo: tipoLixoCheckbox.value,
+        date: new Date()
     })
     .then(
         (doc) => alert("Localização salva", doc.id)
     )
     .catch(console.log)
-    
-
-    getDateAndHour()
 })
